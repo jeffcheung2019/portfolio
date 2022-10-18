681223c12aba39f9d94c54ca2320a6206fe37839
jeff cheung <jeffcheung2019@gmail.com>
2022-10-18 21:04:50 +0800

    switch to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import Layout from 'Layout';
-import { BrowserRouter } from 'react-router-dom';
-import './Styles/Animations.scss'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import theme from 'Theme/MaterialUiTheme'
-import { ParallaxProvider } from 'react-scroll-parallax';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import Layout from "Layout";
+import { HashRouter } from "react-router-dom";
+import "./Styles/Animations.scss";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import theme from "Theme/MaterialUiTheme";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 function App() {
   return (
     <div className="App">
       <ParallaxProvider>
         <ThemeProvider theme={theme}>
-          <BrowserRouter>
+          <HashRouter>
             <Layout />
-          </BrowserRouter>
+          </HashRouter>
         </ThemeProvider>
       </ParallaxProvider>
-
     </div>
   );
 }
